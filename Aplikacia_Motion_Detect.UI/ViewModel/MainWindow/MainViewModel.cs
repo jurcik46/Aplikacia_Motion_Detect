3050dbdb6f40482934419de4c7fc383a8e5ca598
Ján Jurč <jurcik46@gmail.com>
2018-08-10 14:56:32 +0200

    Added file for main windows and maked UI for VideoSettingControl

---
+++ Aplikacia_Motion_Detect.UI/ViewModel/MainWindow/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+namespace Aplikacia_Motion_Detect.UI.ViewModel.MainWindow
+{
+    class MainViewModel : ViewModelBase
+    {
+    }
+}
