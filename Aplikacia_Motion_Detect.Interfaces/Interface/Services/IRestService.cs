672c21ac48c87a16ac669b0da3eb504dc0cb589e
Ján Jurč <jurcik46@gmail.com>
2018-09-25 15:48:31 +0200

    interface for rest api service

---
+++ Aplikacia_Motion_Detect.Interfaces/Interface/Services/IRestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacia_Motion_Detect.Interfaces.Interface.Services
+{
+    public interface IRestService
+    {
+    }
+}
