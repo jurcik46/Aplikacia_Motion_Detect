6fcdae766e0651300f5606eb6aa3764e4fc046e1
Ján Jurč <jurcik46@gmail.com>
2018-09-25 15:46:07 +0200

    created class for rest api service

---
+++ Aplikacia_Motion_Detect.Backend/Service/RestService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplikacia_Motion_Detect.Interfaces.Interface.Services;
+using Aplikacia_Motion_Detect.API;
+using Serilog;
+
+namespace Aplikacia_Motion_Detect.Backend.Service
+{
+
+
+    class RestService : IRestService
+    {
+        private Api _api;
+
+
+        internal Api Api => _api ?? (_api = new Api(/*this.OptionsService.ApiOptions*/));
+        public ILogger Logger => Log.Logger.ForContext<RestService>();
+    }
+}
