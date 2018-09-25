9f8abca1c4541a38868338ee633161689d600e25
Ján Jurč <jurcik46@gmail.com>
2018-09-25 13:32:51 +0200

    api events

---
+++ Aplikacia_Motion_Detect.API/Enums/ApiEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacia_Motion_Detect.API.Enums
+{
+    public enum ApiEvents
+    {
+        Create,
+        ExecuteType,
+        ExecuteTypeError,
+        ExecuteTypeSuccess,
+        GetDocument,
+        UploadDocument
+    }
+}
