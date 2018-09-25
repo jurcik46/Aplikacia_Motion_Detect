f4a6fe0fa694233cf29acde18aeb03694c508804
Ján Jurč <jurcik46@gmail.com>
2018-09-25 15:48:57 +0200

    tested files for design

---
+++ Aplikacia_Motion_Detect.UI/Design/MainWindow/DesignVideoSetting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplikacia_Motion_Detect.Interfaces.Interface.MainWindow;
+
+namespace Aplikacia_Motion_Detect.UI.Design.MainWindow
+{
+    public class DesignVideoSetting : IVideoCaptureSetting
+    {
+        public DesignVideoSetting()
+        {
+
+        }
+
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastError { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Resolution { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PixelFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Frames { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Fps { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Active { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
