53e264289967a6e4d5804cdb40f51ab876c7c9e7
Ján Jurč <jurcik46@gmail.com>
2018-09-27 17:00:20 +0200

    set up method on startup and exit appliaction

--- Aplikacia_Motion_Detect.V1/App.xaml.cs
+++ Aplikacia_Motion_Detect.V1/App.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Serilog;
+using Microsoft.Shell;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using GalaSoft.MvvmLight.Threading;
+using System.Reflection;
+using Aplikacia_Motion_Detect.V1.Extensions;
 
 namespace Aplikacia_Motion_Detect.V1
 {
@@ -13,5 +18,28 @@ namespace Aplikacia_Motion_Detect.V1
     /// </summary>
     public partial class App : Application
     {
+
+        public ILogger Logger => Log.Logger.ForContext<App>();
+
+
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+            DispatcherHelper.Initialize();
+
+            var appName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            var app = Current;
+            var logger = Logger;
+            ApplicationExtensions.InitializeApplication(appName, app, logger);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            ApplicationExtensions.OnExit(Logger);
+        }
+
     }
 }
