cb486c7d465542e926ef09bf9a2995e4e8443024
Ján Jurč <jurcik46@gmail.com>
2018-10-27 12:54:01 +0200

    Finished GUI for Motion Zones Windows

---
+++ Aplikacia_Motion_Detect.UI/Views/MotionZones/VideoDisplayMotionZones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplikacia_Motion_Detect.UI.Views.MotionZones
+{
+    public partial class VideoDisplayMotionZones : UserControl
+    {
+        public VideoDisplayMotionZones()
+        {
+            InitializeComponent();
+        }
+    }
+}
